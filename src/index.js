b55146c352f438f7a1d052bae8095be53779c99f
urraccon <133591634+urraccon@users.noreply.github.com>
2023-12-19 19:23:40 +0200

    made some changes to the firebase configuration file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-//firebase configuration
+// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: 'AIzaSyAKYrF2YoqGKSaSU407C9X91DqaZMAg4q4',
-  authDomain: 'filmoteka-urraccon.firebase.com',
+  authDomain: 'filmoteka-urraccon.firebaseapp.com',
   projectId: 'filmoteka-urraccon',
   storageBucket: 'filmoteka-urraccon.appspot.com',
   messagingSenderId: '712511464775',
   appId: '1:712511464775:web:f28be5eb7fc38469c69862',
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//--------------------------------------------------------------
-
-import { getFirestore, doc, setDoc, getDoc } from 'firebase/firestore';
-
 const db = getFirestore(app);
 console.log(app);
 console.log(db);
